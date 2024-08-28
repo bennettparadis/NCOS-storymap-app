2e13883d15511a7ee7ef6161798e321ada8d262e
Bennett Paradis <60286771+bennettparadis@users.noreply.github.com>
2024-08-28 11:27:21 -0400

    Update main_scatter.py

--- main_scatter.py
+++ main_scatter.py
@@ -46,7 +46,7 @@ fig1 = px.scatter(df,
                 },
                 hover_data=['OS_Name', 'Year'],
                 size_max=15,  # Set the maximum size of the markers
-                height=600,
+                height=400,
                 width=950)
 
 # Add Lowess trendline to the plot
